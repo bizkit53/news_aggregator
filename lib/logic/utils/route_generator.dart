70853161e75bdf6f27804429ee53646c6e32b825
bizkit53 <ciuba.piotr@gmail.com>
2022-07-19 14:41:24 +0200

    Routing improved

--- lib/logic/utils/route_generator.dart
+++ lib/logic/utils/route_generator.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:news_aggregator/presentation/pages/home_page.dart';
+import 'package:news_aggregator/constans/routes.dart';
+import 'package:news_aggregator/presentation/pages/splash_page.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case '/':
+      case splashRoute:
         return MaterialPageRoute(
-          builder: ((context) => const HomePage()),
+          builder: ((context) => const SplashPage()),
         );
+
       default:
         return _errorRoute();
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:news_aggregator/constans/routes.dart';
 import 'package:news_aggregator/firebase_options.dart';
 import 'logic/utils/route_generator.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
-      initialRoute: '/',
+      initialRoute: splashRoute,
       onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
