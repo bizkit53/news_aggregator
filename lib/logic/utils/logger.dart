4889b0d6a6492d291a9fbe39ed5906addbe70fcf
bizkit53 <ciuba.piotr@gmail.com>
2022-09-14 10:01:19 +0200

    Custom logger setup

---
+++ lib/logic/utils/logger.dart
@@ -0,0 +1,24 @@
+import 'package:logger/logger.dart';
+
+final logger = (Type type) => Logger(
+      printer: CustomPrinter(
+        className: type.toString(),
+      ),
+    );
+
+class CustomPrinter extends LogPrinter {
+  final String className;
+
+  CustomPrinter({
+    required this.className,
+  });
+
+  @override
+  List<String> log(LogEvent event) {
+    final AnsiColor? color = PrettyPrinter.levelColors[event.level];
+    final String? emoji = PrettyPrinter.levelEmojis[event.level];
+    final String? message = event.message;
+
+    return [color!('$emoji $className: $message')];
+  }
+}
