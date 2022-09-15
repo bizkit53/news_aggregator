12b6716012c7a86ce304758da9792015fc79b7d0
bizkit53 <ciuba.piotr@gmail.com>
2022-09-15 14:22:48 +0200

    Added custom starting points of the app

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,19 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:news_aggregator/constans/routes.dart';
 import 'package:news_aggregator/firebase_options.dart';
+import 'package:news_aggregator/logic/services/config_reader/config_reader.dart';
 import 'package:news_aggregator/logic/utils/injector.dart';
 import 'package:news_aggregator/logic/utils/route_generator.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
-void main() async {
+/// Setup everything depending on the environment
+Future<void> mainCommon(String env) async {
   WidgetsFlutterBinding.ensureInitialized();
+  await ConfigReader.initialize(env);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   injectorSetup();
   runApp(
     DevicePreview(
