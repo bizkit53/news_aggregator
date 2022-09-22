d186fe3d0a7521bcbe5c177c355f70266f66aecb
bizkit53 <ciuba.piotr@gmail.com>
2022-09-22 20:25:46 +0200

    Skeleton for translations created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:news_aggregator/constans/environment.dart';
 import 'package:news_aggregator/constans/responsive_breakpoints.dart';
 import 'package:news_aggregator/constans/routes.dart';
@@ -37,6 +38,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
       initialRoute: splashRoute,
